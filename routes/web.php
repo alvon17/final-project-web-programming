b050d96d49812834f85892cf443b86a8a5554c02
Chrylus <czar.soecahyo@binus.ac.id>
2022-12-12 12:56:35 +0700

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\AuthController;
 |
 */
 
+Route::get('/', [AuthController::class, 'dashboard']);
 Route::get('/dashboard', [AuthController::class, 'dashboard']);
 Route::get('/login', [AuthController::class, 'login']);
 Route::post('/custom-login', [AuthController::class, 'customLogin']);
